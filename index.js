716a77d9a30eb6900f88edcdd3f3bcf51845f786
Quachell Peele <quachellpeele@gmail.com>
2023-11-06 16:29:10 -0500

    i did it

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+
+  if (distance <= 400) {
+   return "This one is on me!"
+  }
+  else if (distance > 400 && distance <= 2000) {
+   return 'That will be twenty bucks.'
+  } 
+  else if (distance > 2000 && distance < 2500 ) {
+    return 'I will gladly take your thirty bucks.'
+
+  }
+  else if (distance > 2500) {
+    return 'No can do.'
+  }
+} 
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  
+  if (city === 'NYC') {
+    return "Ok, sounds good."
+  }
+  else if (city !== 'NYC') {
+    return "No go."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+ switch (tip) {
+case 'generous':
+  return  "Thank you so much."
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+case 'not as generous':
+  return "Thank you." 
+
+  default :
+  return 'Bye.'
+}
 }
